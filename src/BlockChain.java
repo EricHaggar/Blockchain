e9e3e3d80b8c3a9b00aba2d86c4dd1a45f5a7955
EricHaggar <erichaggar@hotmail.com>
2018-10-19 15:22:27 -0400

    Updated gitignore

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -7,24 +7,25 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 public class BlockChain {
-
+    
     private ArrayList<Block> blocks;
-
+    private ArrayList<Block> validatedBlocks;
+    
     public BlockChain() {
-
-        blocks = new ArrayList<Block>();
-
+        
+        blocks = new ArrayList<>();
+        validatedBlocks = new ArrayList<>();
     }
-
+    
     public static BlockChain fromFile(String fileName) {
-
+        
         BlockChain blockchain = new BlockChain();
-
+        
         try {
-
+            
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+            
             String line;
             int index;
             java.sql.Timestamp timestamp;
@@ -35,11 +36,11 @@ public class BlockChain {
             String previousHash;
             String hash;
             int numOfBlocks = 0;
-
+            
             while ((line = bufferedReader.readLine()) != null) {
-
+                
                 index = Integer.parseInt(line);
-
+                
                 if (numOfBlocks == 0) {
                     previousHash = "00000";
                 } else {
@@ -51,31 +52,31 @@ public class BlockChain {
                 amount = Integer.parseInt(bufferedReader.readLine());
                 nonce = bufferedReader.readLine();
                 hash = bufferedReader.readLine();
-
+                
                 blockchain.blocks.add(new Block(index, timestamp, sender, receiver, amount, nonce, previousHash, hash));
                 numOfBlocks++;
             }
-
+            
             bufferedReader.close();
-
+            
         } catch (IOException e) {
             System.out.print("Error while reading the file!");
         }
-
+        
         return blockchain;
-
+        
     }
-
-
+    
+    
     public void toFile(String fileName) {
-
+        
         try {
-
+            
             FileWriter fileReader = new FileWriter(fileName);
             PrintWriter printWriter = new PrintWriter(fileReader);
-
+            
             for (int i = 0; i < blocks.size(); i++) {
-
+                
                 printWriter.println(blocks.get(i).getIndex());
                 printWriter.println(blocks.get(i).getTimestamp().getTime());
                 printWriter.println(blocks.get(i).getTransaction().getSender());
@@ -84,61 +85,131 @@ public class BlockChain {
                 printWriter.println(blocks.get(i).getNonce());
                 printWriter.println(blocks.get(i).getPreviousHash());
             }
-
+            
             printWriter.close();
-
-
+            
+            
         } catch (IOException e) {
-
             System.out.println("Error while writing to the file!");
         }
     }
-
+    
     public boolean validateBlockchain() {
-
+        
         for (int i = 0; i < blocks.size() - 1; i++) {
-
+            
             Block currentBlock = blocks.get(i);
             Block nextBlock = blocks.get(i + 1);
-
+            
             //checks if indexes are consistent
-            if ((currentBlock.getIndex() != i) || (nextBlock.getIndex() != i+1)) {
+            if ((currentBlock.getIndex() != i) || (nextBlock.getIndex() != i + 1)) {
                 return false;
             }
-
+            
             try {
-
+                
                 //calculates hash using Sha1 algorithm with attributes of the given block
-                String verifiedHash = Sha1.hash(currentBlock.toString(), Sha1.OUT_HEXW).replaceAll("\\s","");
-
+                String currentVerifiedHash = Sha1.hash(currentBlock.toString(), Sha1.OUT_HEXW).replaceAll("\\s", "");
+                
                 //checks if calculated hash is the same as the hash in the block
-                if (!currentBlock.getHash().equals(verifiedHash)) {
+                if (!currentBlock.getHash().equals(currentVerifiedHash)) {
                     return false;
                 }
-
+                
+                if (i == blocks.size() - 2) {
+                    
+                    String nextVerifiedHash = Sha1.hash(nextBlock.toString(), Sha1.OUT_HEXW).replaceAll("\\s", "");
+                    
+                    if (!nextBlock.getHash().equals(nextVerifiedHash)) {
+                        return false;
+                    }
+                    
+                }
+                
             } catch (UnsupportedEncodingException e) {
                 System.out.println("EncodingError");
             }
-
+            
             //checks if the current hash is the same as the previous hash of the next block
             if (!currentBlock.getHash().equals(nextBlock.getPreviousHash())) {
                 return false;
             }
+            
+            //Checking the validity of the transactions
+            if (i == 0) { //the first sender has to be bitcoin
+                
+                if (!currentBlock.getTransaction().getSender().equals("bitcoin")) {
+                    return false;
+                    
+                } else {
+                    validatedBlocks.add(currentBlock);
+                }
+                
+            } else if (!currentBlock.getTransaction().getSender().equals("bitcoin")) {
+                
+                if (i < blocks.size() - 2) {
+                    
+                    if (getBalance(currentBlock.getTransaction().getSender()) < currentBlock.getTransaction().getAmount()) {
+                        return false;
+                        
+                    } else {
+                        validatedBlocks.add(currentBlock);
+                    }
+                    
+                } else {
+                    
+                    if (getBalance(currentBlock.getTransaction().getSender()) < currentBlock.getTransaction().getAmount()) {
+                        return false;
+                        
+                    } else {
+                        validatedBlocks.add(currentBlock);
+                    }
+                    
+                    if (getBalance(nextBlock.getTransaction().getSender()) < nextBlock.getTransaction().getAmount()) {
+                        return false;
+                        
+                    } else {
+                        validatedBlocks.add(nextBlock);
+                    }
+                    
+                }
+                
+            } else {
+                validatedBlocks.add(currentBlock);
+            }
+            
         }
-
+        
         return true;
-
     }
-
+    
+    public int getBalance(String username) {
+        
+        int userBalance = 0;
+        
+        for (int i = 0; i < validatedBlocks.size(); i++) {
+            
+            if (validatedBlocks.get(i).getTransaction().getReceiver().equals(username)) {
+                
+                userBalance += validatedBlocks.get(i).getTransaction().getAmount();
+                
+            } else if (validatedBlocks.get(i).getTransaction().getSender().equals(username)) {
+                
+                userBalance -= validatedBlocks.get(i).getTransaction().getAmount();
+            }
+            
+        }
+        
+        return userBalance;
+    }
+    
     public static void main(String[] args) {
-
+        
         BlockChain blockchain = fromFile("bitcoinBank.txt");
-
+        
         blockchain.validateBlockchain();
-
+        
         blockchain.toFile("output.txt");
-
-
     }
 }
 
